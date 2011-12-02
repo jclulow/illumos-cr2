5e2859236739170147d99935134ad33405849327
Joshua M. Clulow <josh@jmctech.com.au>
2011-12-02 11:46:26 +1100

    config/sessions in redis

--- app.js
+++ app.js
@@ -5,6 +5,15 @@
 
 var express = require('express');
 var routes = require('./routes');
+/*var config = require('express-configure');*/
+var redis = require('redis');
+var crypto = require('crypto');
+var util = require('util');
+var async = require('async');
+
+var RedisStore = require('connect-redis')(express);
+
+var rc = redis.createClient();
 
 var crmgr = require('./crmgr');
 
@@ -12,28 +21,133 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
+async.waterfall([
+  function(next) {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    next();
+  },
+  function(next) {
+    // select redis database
+    rc.select(0, next);
+  },
+  function(obj, next) {
+    // request settings hash from redis
+    rc.hgetall('illumos:cr:settings', next);
+  },
+  function(obj, next) {
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        var val = obj[key];
+        if (val === 'true') {
+          val = true;
+        } else if (val === 'false') {
+          val = false;
+        } else if (!isNaN(val)) {
+          val = Number(val);
+	}
+        console.log('setting [ ' + key + ' ] = ' + util.inspect(val));
+        app.set(key, obj[key]);
+      }
+    }
+    app.use(express.session({
+      secret: app.settings.session_secret,
+      store: new RedisStore()
+    }));
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+    if (app.settings.show_errors) {
+      app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    } else {
+      app.use(express.errorHandler());
+    }
+    /* pre-initialise locals for template: */
+    app.all('*', local_populator);
+    /* application routes: */
+    app.get('/', routes.index);
+    app.get('/cr/:id?', crmgr.get);
+    app.post('/cr/new', crmgr.post);
+    app.get('/login', handle_login_get);
+    app.post('/login', handle_login);
+    app.get('/logout', handle_logout);
+    next();
+  }],
+  // error handler / final callback:
+  function last(err) {
+    if (err) {
+      console.log("STARTUP ERROR: " + util.inspect(err));
+      process.exit(1);
+    } else {
+      console.log("LISTENING ON PORT: " + app.settings.port);
+      app.listen(app.settings.port);
+    }
+  }
+);
 
 // Routes
 
-app.get('/', routes.index);
+function local_populator(req, res, next) {
+	var links = [];
+	res.local('links', [
+		{ active: true, path: '/cr', title: 'Browse Reviews' },
+		{ active: false, path: '/my/cr', title: 'My Reviews' },
+		{ active: false, path: '/cr/new', title: 'New Review' }
+	]);
+	if (req.session && req.session.user)
+		res.local('user', req.session.user);
+	else
+		res.local('user', {});
+	res.local('title', "XXX DEFAULT TITLE");
+	next();
+}
 
-app.get('/cr/:id?', crmgr.get);
+function shasum(str) {
+	var sha = crypto.createHash('sha1');
+	sha.update(str);
+	return (sha.digest('hex'));
+}
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+function handle_login_get(req, res, next) {
+	var opts = { title: 'Sign In' };
+	if (req.session.loginerror)
+		opts.loginerror = req.session.loginerror;
+	res.render('login', opts);
+}
+
+function handle_login(req, res, next) {
+	console.log("POST /login: " + util.inspect(req.body));
+	var username = req.body.top_user || req.body.login_user;
+	var password = req.body.top_pass || req.body.login_pass;
+	if (username && password) {
+		rc.hgetall('illumos:user:' + username, function (err, obj) {
+			if (err) {
+				console.log("error: " + util.inspect(err));
+				req.session.loginerror = 'Sorry, please try again!';
+				res.redirect('/login');
+			} else {
+				console.log("user: " + util.inspect(obj));
+				if (shasum(password) === obj.password) {
+					delete obj.password;
+					req.session.user = obj;
+					res.redirect('/');
+					//res.render('user', { title: 'User', user: obj });
+				} else {
+					req.session.loginerror = 'Sorry, please try again!';
+					res.redirect('/login');
+				}
+			}
+		});
+	} else {
+		req.session.loginerror = 'Sorry, you must enter a username and password!';
+		res.redirect('/login');
+	}
+}
+
+function handle_logout(req, res, next) {
+	console.log('GET /logout');
+	req.session.destroy();
+	return (res.redirect('/'));
+}
--- crmgr.js
+++ crmgr.js
@@ -1,6 +1,69 @@
+/*
+ * Code Review routes:
+ *
+ *   /cr/:id?
+ *
+ */
 
+var async = require('async');
+var fs = require('fs');
 
-exports.get = function crmgr_get(req, res, next) {
-	return (res.redirect('/'));
+function CodeReviewManager(redis, storepath) {
+  if (!redis)
+    throw new Error('need a redis connection');
+  if (!storepath)
+    throw new Error('need a store path');
+  this._redis = redis;
+  this._storepath = storepath;
 }
 
+function createUpload(username, callback) {
+  var _this = this;
+  var cr = { username: username };
+  var rkey;
+  async.waterfall([
+    function(next) {
+      // get ID from redis
+      _this._redis.incr("illumos:cr:next_upload_number", next);
+    },
+    function(uplnum, next) {
+      rkey = 'illumos:cr:review:' + uplnum;
+      cr.upload_number = uplnum;
+      // store prelim information in database
+      // XXX should wrap DEL + HMSET in MULTI/EXEC to be sure?
+      _this._redis.hmset(rkey, cr, next);
+    },
+    function(next) {
+      // create directory in filesystem
+      // XXX use node path routines
+      cr.dir = _this._storepath + '/' + uplnum;
+      fs.mkdir(cr.dir, 0700, next);
+    },
+    function(next) {
+      // update database
+      _this._redis.hmset(rkey, cr, next);
+    }
+  ],
+    function(err) {
+      if (err) {
+        callback(error);
+      } else {
+        callback(null, cr);
+      }
+    }
+  );
+}
+
+exports.get = function crmgr_get(req, res, next) {
+  if (req.params.id) {
+    res.render('review_detail', { title: 'Review #' + req.params.id });
+  } else {
+    res.render('review_list', { title: 'Browse Reviews' });
+  }
+}
+
+exports.post = function crmgr_post(req, res, next) {
+}
+
+exports.CodeReviewManager = CodeReviewManager;
+exports.CodeReviewManager.prototype.createUpload = createUpload;
